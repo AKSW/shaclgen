c1c8f5b9c7c7fdcc7ec6e4e5e5ac366632edf04c
alexis mcclimans <alexiskm@uw.edu>
2019-11-05 08:08:36 -0800

    push files

---
+++ install.sh
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 14:08:11 2019
+
+@author: alexi
+"""
+
+pip install -e .
---
+++ shapegen/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 14:08:49 2019
+
+@author: alexi
+"""
+
