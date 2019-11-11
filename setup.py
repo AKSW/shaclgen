a187d2709ebbd7d80d2420f806626509edd31445
alexis mcclimans <alexiskm@uw.edu>
2019-11-11 09:24:22 -0800

    add manifest

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 #%%
 from setuptools import setup
+
+
+with open('README.md') as readme:
+    l_description = readme.read()
+with open('requirements.txt') as reqs:
+    requirements = reqs.read()
+
 setup(
-    name = 'shaclgen',
-    version = '0.1.0',
+    name = 'shaclgen_test',
+    version = '0.1.1',
     packages = ['shaclgen'],
     description='Shacl graph generator',
+    long_description=l_description,
     author='Alexis Keely',
+    url='https://github.com/alexiskeely/shaclgen',
     author_email='alexiskm@uw.com',
-    license='MIT',
+    install_requires=requirements,
+
+    keywords=['Linked Data', 'Semantic Web', 'Python',
+              'SHACL', 'Shapes', 'Schema', 'Validate'],
+    license='LICENSE.txt',
+    classifiers=[
+                'Programming Language :: Python :: 3',
+
+    ],
     entry_points = {
         'console_scripts': [
             'shaclgen = shaclgen.__main__:main'
