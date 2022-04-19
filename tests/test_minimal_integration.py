ec7f3773782197432f61582b276fa3985eb776a8
Natanael Arndt <natanael.arndt@eccenca.com>
2022-04-19 17:52:54 +0200

    Add test with node kind BNode

--- tests/test_minimal_integration.py
+++ tests/test_minimal_integration.py
@@ -17,9 +17,7 @@ def test_create_shape():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    assertAskQuery(
-        shacl_graph,
-        """
+    assertAskQuery(shacl_graph, """
     prefix sh: <http://www.w3.org/ns/shacl#>
     prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     ask {
@@ -37,8 +35,7 @@ def test_create_shape():
             sh:nodeKind sh:IRI ;
             sh:path <urn:test:obj_property> .
     }
-    """,
-    )
+    """)
 
 
 def test_two_classes():
@@ -55,9 +52,7 @@ def test_two_classes():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    assertAskQuery(
-        shacl_graph,
-        """
+    assertAskQuery(shacl_graph, """
     prefix sh: <http://www.w3.org/ns/shacl#>
     prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     ask {
@@ -69,8 +64,7 @@ def test_two_classes():
             sh:nodeKind sh:BlankNodeOrIRI ;
             sh:targetClass <urn:test:OtherClass> .
     }
-    """,
-    )
+    """)
 
 
 def test_object_class():
@@ -87,9 +81,7 @@ def test_object_class():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    assertAskQuery(
-        shacl_graph,
-        """
+    assertAskQuery(shacl_graph, """
     prefix sh: <http://www.w3.org/ns/shacl#>
     prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     ask {
@@ -107,8 +99,7 @@ def test_object_class():
             sh:nodeKind sh:BlankNodeOrIRI ;
             sh:targetClass <urn:test:OtherClass> .
     }
-    """,
-    )
+    """)
 
 
 def test_two_object_classes():
@@ -125,9 +116,7 @@ def test_two_object_classes():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    assertAskQuery(
-        shacl_graph,
-        """
+    assertAskQuery(shacl_graph, """
     prefix sh: <http://www.w3.org/ns/shacl#>
     prefix xsd: <http://www.w3.org/2001/XMLSchema#>
     ask {
@@ -150,5 +139,33 @@ def test_two_object_classes():
             sh:or/rdf:rest*/rdf:first/sh:class <urn:test:OtherClass2> ;
             sh:path <urn:test:obj_property> .
     }
-    """,
-    )
+    """)
+
+
+def test_object_blank_node():
+    source_graph = Graph()
+
+    data = """
+    <urn:test:resource> a <urn:test:Class> ;
+    <urn:test:obj_property> [] .
+    """
+
+    source_graph.parse(data=data, format="turtle")
+
+    extraction_graph = data_graph(source_graph)
+    shacl_graph = extraction_graph.gen_graph()
+
+    assertAskQuery(shacl_graph, """
+    prefix sh: <http://www.w3.org/ns/shacl#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    ask {
+        ?nodeShapeA a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:property ?property ;
+            sh:targetClass <urn:test:Class> .
+
+        ?property a sh:PropertyShape ;
+            sh:nodeKind sh:BlankNode ;
+            sh:path <urn:test:obj_property> .
+    }
+    """)
