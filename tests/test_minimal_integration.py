9f330896dbe75fcf507c5e87de712a45151b54f1
Natanael Arndt <natanael.arndt@eccenca.com>
2022-04-19 17:15:02 +0200

    Add assertions to the tests

--- tests/test_minimal_integration.py
+++ tests/test_minimal_integration.py
@@ -2,6 +2,14 @@ from rdflib import Graph
 from shaclgen.shaclgen import data_graph
 
 
+def assertAskQuery(graph, query):
+    result = graph.query(query)
+    if not result.askAnswer:
+        serialization = graph.serialize(format="turtle")
+        explanation = "The actual resulting shacl graph is:\n" + serialization
+    assert result.askAnswer, explanation
+
+
 def test_create_shape():
     source_graph = Graph()
 
@@ -16,7 +24,25 @@ def test_create_shape():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    print(shacl_graph.serialize(format="turtle"))
+    assertAskQuery(shacl_graph, """
+    prefix sh: <http://www.w3.org/ns/shacl#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    ask {
+        ?nodeShape a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:Class> ;
+            sh:property ?propA, ?propB .
+
+        ?propA a sh:PropertyShape ;
+            sh:datatype xsd:string ;
+            sh:nodeKind sh:Literal ;
+            sh:path <urn:test:lit_property> .
+            ?ropB a sh:PropertyShape ;
+
+            sh:nodeKind sh:IRI ;
+            sh:path <urn:test:obj_property> .
+    }
+    """)
 
 
 def test_two_classes():
@@ -33,7 +59,19 @@ def test_two_classes():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    print(shacl_graph.serialize(format="turtle"))
+    assertAskQuery(shacl_graph, """
+    prefix sh: <http://www.w3.org/ns/shacl#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    ask {
+        ?nodeShapeA a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:Class> .
+
+        ?nodeShapeB a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:OtherClass> .
+    }
+    """)
 
 
 def test_object_class():
@@ -50,7 +88,25 @@ def test_object_class():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    print(shacl_graph.serialize(format="turtle"))
+    assertAskQuery(shacl_graph, """
+    prefix sh: <http://www.w3.org/ns/shacl#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    ask {
+        ?nodeShapeA a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:property ?property ;
+            sh:targetClass <urn:test:Class> .
+
+        ?property a sh:PropertyShape ;
+            sh:class <urn:test:OtherClass> ;
+            sh:nodeKind sh:IRI ;
+            sh:path <urn:test:obj_property> .
+
+        ?nodeShapeB a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:OtherClass> .
+    }
+    """)
 
 
 def test_two_object_classes():
@@ -67,4 +123,27 @@ def test_two_object_classes():
     extraction_graph = data_graph(source_graph)
     shacl_graph = extraction_graph.gen_graph()
 
-    print(shacl_graph.serialize(format="turtle"))
+    assertAskQuery(shacl_graph, """
+    prefix sh: <http://www.w3.org/ns/shacl#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    ask {
+        ?nodeShapeA a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:property ?property ;
+            sh:targetClass <urn:test:Class> .
+
+        ?nodeShapeB a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:OtherClass> .
+
+        ?nodeShapeC a sh:NodeShape ;
+            sh:nodeKind sh:BlankNodeOrIRI ;
+            sh:targetClass <urn:test:OtherClass2> .
+
+        ?property a sh:PropertyShape ;
+            sh:nodeKind sh:IRI ;
+            sh:or/rdf:rest*/rdf:first/sh:class <urn:test:OtherClass> ;
+            sh:or/rdf:rest*/rdf:first/sh:class <urn:test:OtherClass2> ;
+            sh:path <urn:test:obj_property> .
+    }
+    """)
